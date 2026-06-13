15f886822b0d193ad4fab6b30d91d950e235ab7e
Harshini2721 <harshinimundlamuri7521@gmail.com>
2026-06-13 16:46:57 +0530

    Implement encryption and decryption functions

---
+++ server/utils/encrypt.js
@@ -0,0 +1,24 @@
+const crypto = require("crypto");
+
+const ALGORITHM = "aes-256-cbc";
+const SECRET_KEY = process.env.ENCRYPTION_KEY;
+const IV_LENGTH = 16;
+
+function encrypt(text) {
+  const iv = crypto.randomBytes(IV_LENGTH);
+  const cipher = crypto.createCipheriv(ALGORITHM, Buffer.from(SECRET_KEY), iv);
+  let encrypted = cipher.update(text, "utf8", "hex");
+  encrypted += cipher.final("hex");
+  return iv.toString("hex") + ":" + encrypted;
+}
+
+function decrypt(text) {
+  const [ivHex, encryptedHex] = text.split(":");
+  const iv = Buffer.from(ivHex, "hex");
+  const decipher = crypto.createDecipheriv(ALGORITHM, Buffer.from(SECRET_KEY), iv);
+  let decrypted = decipher.update(encryptedHex, "hex", "utf8");
+  decrypted += decipher.final("utf8");
+  return decrypted;
+}
+
+module.exports = { encrypt, decrypt };
